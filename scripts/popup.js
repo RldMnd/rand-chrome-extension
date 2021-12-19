e12bc4bc7e33c4d78fdd38ad208197f1dce1990f
Lyudmila Romanova <lr@motus-group.ru>
2021-12-19 21:06:21 +0500

    Сборка куков

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,18 +1,23 @@
 let btn=document.getElementById("startButton");
 let data=document.getElementById("data");
+let cooStr="";
+const fields="desktop_session_key:desktop_session_key.sig:user-geo-region-id:user-geo-country-id:gdpr:yandexuid:yuidss:PHPSESSID:yandex_gid:_csrf_csrf_token:_csrf:location:mda_exp_enabled:mustsee_sort_v5:yandex_login:uid:_ym_uid:mykp_button:mustseeFormat:my_perpages:i:crookie:cmtchd:ys:yp:ymex:user_country:tc:mobile:ya_sess_id:mda2_beacon:yandex_plus_metrika_cookie:_ym_isad:sso_status:_ym_visorc:_ym_d";
+let fieldsArr=[];
 
 btn.addEventListener("click",
-    function (event) {        
-        console.log("Sup! 3");
+    function (event) {
+        fieldsArr=fields.split(":");
         chrome.cookies.getAll(
-            {domain:".kinopoisk.ru",
-        name:"PHPSESSID"},
+            {
+                domain:".kinopoisk.ru"
+            },
             function (cookies) {
-                console.log(cookies);
-                data.textContent=cookies.length;
-                cookies.forEach(element => {
-                    data.textContent=element.name;
-                });
+                data.textContent=`Всего куков ${cookies.length}`;
+                cookies=cookies.filter(el=>fieldsArr.indexOf(el.name)>-1);
+                cookies=cookies.map(el=>`${el.name}=${el.value}`);
+                cooStr=cookies.join(";");
             });
+        
+            console.log(cooStr);
         }
       );
